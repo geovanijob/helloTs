8c6e17aa80e9b82f109359e944ad77d9bdd41fc1
Munif Gebara Junior <munifgebarajunior@MacBook-Pro-de-Munif.local>
2019-03-09 10:56:38 -0300

    aula na alfa

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+var a = 50;
+var v = ['arroz', 'feijao', 'pao'];
+v.forEach(function (s) { return console.log(s.toUpperCase()); });
+console.log("Bom dia!!!");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let a=50;
+
+let v=['arroz','feijao','pao'];
+
+v.forEach(s=>console.log(s.toUpperCase()));
+
+
+
+console.log("Bom dia!!!");
+
